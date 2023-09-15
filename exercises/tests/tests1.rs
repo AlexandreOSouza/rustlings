b683bb7b4b479b067349b81e12cd49edc3317bbd
Alexandre <alexandre.o.souza12@gmail.com>
2023-09-15 10:33:48 -0300

    chore: resulutions

---
+++ exercises/tests/tests1.rs
@@ -0,0 +1,22 @@
+// tests1.rs
+// Tests are important to ensure that your code does what you think it should do.
+// Tests can be run on this file with the following command:
+// rustlings run tests1
+
+// This test has a problem with it -- make the test compile! Make the test
+// pass! Make the test fail!
+// Execute `rustlings hint tests1` or use the `hint` watch subcommand for a hint.
+
+fn return_true() -> bool {
+    true
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::return_true;
+
+    #[test]
+    fn you_can_assert() {
+        assert!(return_true(), "{}", true);
+    }
+}
